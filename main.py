432f7c154c465476aca3c9a080052dc8ad84499f
lambda-stock <yigitdogann47@gmail.com>
2021-09-17 18:57:13 +0300

    Errors fixed.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import os
 
 app = Client(
-    os.environ.get("SESSION_NAME"), bot_token = os.environ.get("BOT_TOKEN"), api_id = os.environ.get("API_ID"), api_hash = os.environ.get("API_HASH")
+    api_id = os.environ.get("API_ID"), api_hash = os.environ.get("API_HASH"), os.environ.get("SESSION_NAME"), bot_token = os.environ.get("BOT_TOKEN")
 )
 
 print(os.environ.get("API_ID"))
